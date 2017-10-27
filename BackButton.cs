ef71dacbcae511ee2ef9bfaa3339ff29c39fd2d0
Kenneth Michael Stewart <stewa584@msu.edu>
2017-10-27 14:43:02 -0400

    Added the tutorial controller class to scripts

---
+++ BackButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class BackButton : MonoBehaviour {
+
+    public GameObject global;
+	// Use this for initialization
+	public void Back()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
